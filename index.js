54e0351af2d0bceeba85fc3c8a262a91e9fd454e
vasyl-ws <vboychuk@glenflow.com>
2025-04-29 13:46:07 +0300

    updated message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure Container Apps updated!');
+  res.send('Hello from Azure Container Apps updated Tue 29 Apr 2025!');
 });
 
 app.listen(port, () => {
